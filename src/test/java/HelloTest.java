105ce75e3c15b3f70d40e65de97008ae270d01ec
meneshazir <meneshazir@gmail.com>
2021-06-11 16:33:52 +0100

    Added second update

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,6 +1,9 @@
 public class HelloTest {
     public static void main(String[] args) {
 
+        System.out.println("Hello Test");
+        System.out.println("About to share project on GitHub");
+
 
     }
 }
